70186c46ecd278e143d1ac5b166597292adda816
GhRafayel <ghazarysnrafayel@gmail.com>
2026-01-03 03:46:32 +0400

    Sat Jan 03 03:46 at home

--- output/src/ConfigPars.d
+++
@@ -1,2 +0,0 @@
-output/src/ConfigPars.o: src/ConfigPars.cpp src/../hpp/ConfigPars.hpp \
- src/../hpp/StringUtils.hpp src/../hpp/Location.hpp src/../hpp/Server.hpp
--- output/src/Request.d
+++
@@ -1,3 +0,0 @@
-output/src/Request.o: src/Request.cpp src/../hpp/Request.hpp \
- src/../hpp/StringUtils.hpp src/../hpp/Client.hpp src/../hpp/Server.hpp \
- src/../hpp/Location.hpp
